bc9ee6120b667e14c5235696ac3a5c51381cd543
Yosuke Nyamamoto <nyamamoto@example.com>
2026-06-25 15:30:52 +0900

    auto-save: 2026-06-25 15:30

--- Project/tokaiAgentTest/force-app/main/default/lwc/agentforceInputLauncherShort/agentforceInputLauncherShort.js
+++ Project/tokaiAgentTest/force-app/main/default/lwc/agentforceInputLauncherShort/agentforceInputLauncherShort.js
@@ -1,12 +1,24 @@
-import { LightningElement, api, track } from 'lwc';
+import { LightningElement, api, track, wire } from 'lwc';
 import { open, execute } from 'lightning/accApi';
+import getBotId from '@salesforce/apex/EmployeeAgentController.getBotId';
 
 export default class AgentforceInputLauncherShort extends LightningElement {
     @api titleText = 'Agentforce がお手伝いします';
     @api placeholderText = 'Agentforce に質問する...';
-    @api botId;
+    @api botId; // App Builder から DeveloperName が渡される
 
     @track inputText = '';
+    _resolvedBotId;
+
+    // DeveloperName から BotDefinition.Id を解決
+    @wire(getBotId, { developerName: '$botId' })
+    wiredBotId({ data, error }) {
+        if (data) {
+            this._resolvedBotId = data;
+        } else if (error) {
+            console.error('[AgentforceInputLauncherShort] getBotId error:', error);
+        }
+    }
 
     handleInput(event) {
         this.inputText = event.target.value;
@@ -25,10 +37,11 @@ export default class AgentforceInputLauncherShort extends LightningElement {
         const input = this.template.querySelector('.agent-input');
         if (input) input.value = '';
 
+        const id = this._resolvedBotId;
         if (message) {
-            await execute(message, this.botId);
+            await execute(message, id);
         } else {
-            await open(this.botId);
+            await open(id);
         }
     }
 }
